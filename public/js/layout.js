d6a9a1ceaec3ac4e6e2e37a26c65126fe1d3ebf9
kouta0530 <16jk047@ymklab.net>
2020-05-31 22:42:44 +0900

    バニラjsの書き方に変更

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,9 +1,9 @@
+/*
 $(document).on("click","#add",function(){
     const name = $("#name").val();
     judgeMapPoint(name);
-    
-});
-
+});*/
+/*
 $(document).on("click","li",function(){
     const name = $(this).text();
     const mi = searchMapDataIndex(name);
@@ -13,7 +13,8 @@ $(document).on("click","li",function(){
     initMap(Options);
 
 });
-
+*/
+/*
 $(document).on("click","#delete",function(){
     const name = $(this).text();
     const mi = searchMapDataIndex(name);
@@ -31,6 +32,7 @@ $("#schedule").click(function(){
     $("#list").find("li").remove();
     
     mapArray.sort(sortMapArray);
+    
     for(let index in mapArray){
         const map = mapArray[index];
         writeMapData(map);
@@ -38,8 +40,62 @@ $("#schedule").click(function(){
 
     alert("観光地の順番を並び替えました");
 });
+*/
+
+const add = document.getElementById("add");
+const all_delete = document.getElementById("all_delete");
+const schedule = document.getElementById("schedule");
+
+add.addEventListener("click",e => {
+    const name = document.getElementById("name");
+    judgeMapPoint(name.value);
+
+});
+
+all_delete.addEventListener("click",e =>{
+    mapArray.length = 0;
+    delete_list();
+});
+
+schedule.addEventListener("click",e =>{
+    delete_list();
+    mapArray.sort(sortMapArray);
+    
+    for(let index in mapArray){
+        const map = mapArray[index];
+        writeMapData(map);
+    }
+    alert("観光地の順番を並び替えました");
+});
 
 /*リストを描写する*/
 const writeMapData = (map) =>{
-    $("#list").append('<li>'+ map.getName() /*+ '<button id = "delete">削除</button></li>'*/);
+    const list = document.getElementById("list");
+    let new_map_data  = document.createElement("li");
+    
+    new_map_data.textContent = map.getName();
+    new_map_data.addEventListener("click",e=>{
+        show_map_Data(new_map_data.textContent);
+    });
+    
+    list.appendChild(new_map_data);
+
+    /*$("#list").append('<li>'+ map.getName() /*+ '<button id = "delete">削除</button></li>');*/
+}
+/*リストを空にする*/ 
+const delete_list = () =>{
+    let list = document.getElementById("list");
+    while(list.lastChild){
+        list.removeChild(list.lastChild);
+    }
+}
+
+/*クリックした地名を表示する*/
+const show_map_Data = (mapName) =>{
+    const name = mapName;
+    const mi = searchMapDataIndex(name);
+    const map = mapArray[mi];
+    
+    Options.center = map.getPoint();
+    initMap(Options);
 }
