1844116a2bf0d8fd81b351e222bf5e72ff17bc42
kouta0530 <16jk047@ymklab.net>
2020-06-12 09:55:18 +0900

    Vueの書き方に変更

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,49 @@
+let Options = {
+    zoom: 15,      //地図の縮尺値
+    center: {lat: 35.6811673, lng: 139.7670516},    //地図の中心座標
+    mapTypeId: 'roadmap'   //地図の種類
+};
 
-        /*地名->地名+座標データ*/
-        const judgeMapPoint = (name) =>{
-            const geocoder = new google.maps.Geocoder();
+/*マップの生成、埋め込み*/
+function initMap(Options){
+    const target = document.getElementById('map')
+    const map = new google.maps.Map(target, Options);
+}
+
+
+/*地名->地名+座標データ*/
+const judgeMapPoint = (name,mapArray) =>{
+    const geocoder = new google.maps.Geocoder();
             
-            geocoder.geocode({ address: name },function(results,status){           
-                if (status === google.maps.GeocoderStatus.OK) {
-                    const point = {"lat":results[0].geometry.location.lat(), 
-                                    "lng":results[0].geometry.location.lng()};     
-                    
-                    createMapDataPushMapArray(name,point);
-                }
-                else{
-                    alert("not in " + name);
-                };
-            });
+    geocoder.geocode({ address: name },function(results,status){           
+        if (status === google.maps.GeocoderStatus.OK) {
+            const point = {"lat":results[0].geometry.location.lat(), "lng":results[0].geometry.location.lng()};     
+            createMapDataPushArray(name,point,mapArray);
         }
+        else{
+            alert('not in:' + name);
+        };
+    });
+}
 
-        /*観光地のデータをリストに加える、リストの描写を更新*/
-        const createMapDataPushMapArray =(name,point)=>{
-            const map_data = new MapData(name,point);
-            map_data.setIndex(mapArray.length);
-            map_data.setRoute();
-
-            mapArray.push(map_data);
-            writeMapData(map_data);
-        }
+/*観光地のデータをリストに加える*/
+const createMapDataPushArray =(name,point,mapArray)=>{
+    const map_data = new MapData(name,point);
+    map_data.setRoute();
+            
+    mapArray.push(map_data);
+}
         
-        /*観光地のリストを並び替える*/
-        const sortMapArray = (map1,map2) => {
-            if(map1.route > map2.route){
-                return -1;
-            }
-            else{
-                return 1;
-            }
-        }
+/*観光地のリストを並び替える*/
+const sortMapArray = (map1,map2) => {
+    if(map1.route > map2.route){
+        return -1;
+    }
+    else{
+        return 1;
+    }
+}
 
-        /*検索地名があるか照合する (name) => -1 or index　name in MapArray*/ 
-        const searchMapDataIndex = (name) => {
-            for(let map_data of mapArray){
-                if(map_data.getName() == name){
-                   return mapArray.indexOf(map_data);
-                }
-            }
-            return -1;
-        }
+
 
 
---
+++ public/js/side.js
@@ -0,0 +1,38 @@
+var sideMenu = new Vue({
+    el:".side",
+    data:{
+        name: "",
+        mapArray:[]
+    },
+    methods: {
+        schedule(){
+            this.mapArray.sort(sortMapArray);
+            alert("並び替えました");
+        },
+        all_delete(){
+            this.mapArray.splice(0,this.mapArray.length);
+        },
+        add(name){
+            this.name = name;
+            judgeMapPoint(this.name,this.mapArray);
+        },
+        select(name){
+            this.name = name;
+            const mi = this.searchMapDataIndex();
+            const map = this.mapArray[mi];
+
+            Options.center = map.getPoint();
+            initMap(Options);
+            
+        },
+        /*検索地名があるか照合する (name) => -1 or index　name in MapArray*/ 
+        searchMapDataIndex(){
+            for(let map_data of this.mapArray){
+                if(map_data.getName() == this.name){
+                    return this.mapArray.indexOf(map_data);
+                }
+            }
+            return -1;
+        }
+    }
+});
